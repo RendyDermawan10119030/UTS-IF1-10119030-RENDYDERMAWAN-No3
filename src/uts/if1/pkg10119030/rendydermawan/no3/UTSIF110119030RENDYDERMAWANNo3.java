052e42c36e49aa9f2732c20e113e0242d198fdfd
RendyDermawan10119030 <rendy.10119030@mahasiswa.unikom.ac.id>
2020-12-04 22:26:49 +0700

    First commit

---
+++ src/uts/if1/pkg10119030/rendydermawan/no3/UTSIF110119030RENDYDERMAWANNo3.java
@@ -0,0 +1,15 @@
+package uts.if1.pkg10119030.rendydermawan.no3;
+/*
+* Nama                 :Rendy Dermawan
+* Kelas                :IF-1
+* NIM                  :10119030
+* Deskripsi Program    :Soal UTS No 1 bersisi Program
+*/
+public class UTSIF110119030RENDYDERMAWANNo3 {
+        
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
